fdaca763fa7b4b087c6453e96b46ac80ae52d537
jokkeiii <54310059+jokkeiii@users.noreply.github.com>
2024-01-14 18:34:57 +0200

    ex02 done

---
+++ wk03/ex03/utility.h
@@ -0,0 +1,25 @@
+// utility.h
+#pragma once
+
+#include <ctype.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <time.h>
+using namespace std;
+
+enum Error_code {
+  success,
+  fail,
+  utility_range_error,
+  underflow,
+  overflow,
+  fatal,
+  not_present,
+  duplicate_error,
+  entry_inserted,
+  entry_found,
+  internal_error
+};
+
+bool user_says_yes();
