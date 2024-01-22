238862afcadc7c926cfff6c26d583615aeac404d
jokkeiii <54310059+jokkeiii@users.noreply.github.com>
2024-01-22 06:48:33 +0200

    wk03 ex1 c

--- wk03/ex01/main.cpp
+++ wk03/ex01/main.cpp
@@ -12,7 +12,8 @@ Uses:  The STL class stack and its methods
 {
   int n;
   char item;
-  stack<char> chars; //  declares and initializes a stack of characters
+  stack<char> char_stack,
+      reverse_stack; //  declares and initializes a stack of characters
 
   cout << " Type in an integer n followed by n characters." << endl
        << " The characters will be printed in reverse order." << endl;
@@ -20,13 +21,30 @@ Uses:  The STL class stack and its methods
 
   for (int i = 0; i < n; i++) {
     cin >> item;
-    chars.push(item);
+    char_stack.push(item);
+  }
+  cout << endl << endl;
+
+  stack<char> temp_stack = char_stack;
+
+  while (!temp_stack.empty()) {
+    reverse_stack.push(temp_stack.top());
+    cout << temp_stack.top();
+    temp_stack.pop();
   }
 
-  cout << endl << endl;
-  while (!chars.empty()) {
-    cout << chars.top();
-    chars.pop();
-  }
   cout << endl;
+
+  bool is_palindrome = true;
+  while (!char_stack.empty() && !reverse_stack.empty()) {
+    if (char_stack.top() != reverse_stack.top()) {
+      is_palindrome = false;
+    }
+    char_stack.pop();
+    reverse_stack.pop();
+  }
+
+  if (is_palindrome) {
+    cout << "Given characters are a palindrome" << endl;
+  }
 }
