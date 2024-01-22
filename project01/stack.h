e9eb166021d9b5d6d7b173f1799330c9d2bf3ab2
jokkeiii <54310059+jokkeiii@users.noreply.github.com>
2024-01-22 15:35:16 +0200

    project01 postfix calc p1 done

---
+++ project01/stack.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "utility.h"
+
+typedef double Stack_entry;
+const int maxstack = 10; //  small value for testing
+
+class Stack {
+public:
+  Stack();
+  bool empty() const;
+  Error_code pop();
+  Error_code top(Stack_entry &item) const;
+  Error_code push(const Stack_entry &item);
+
+private:
+  int count;
+  Stack_entry entry[maxstack];
+};
